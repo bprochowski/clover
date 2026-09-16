320d565bf046b3086f0cae9ee120aa5ed0ed32a3
Bartosz Prochowski <bprochowski@proton.me>
2026-09-16 22:15:17 +0200

    Add navigation methods for SourceCursor

--- src/Support/SourceCursor.cpp
+++ src/Support/SourceCursor.cpp
@@ -1,6 +1,41 @@
 #include "SourceCursor.hpp"
 
+#include <algorithm>
+
+namespace {
+constexpr char source_end_mark = '\0';
+}
+
 SourceCursor::SourceCursor(std::string_view const source)
     : source_(source)
+    , position_(0)
 {
+}
+
+char SourceCursor::peek(uint8_t const offset)
+{
+    uint16_t position_ahead = position_ + offset;
+    if (position_ahead < source_.length()) {
+        return source_.at(position_ahead);
+    }
+
+    return source_end_mark;
+}
+
+char SourceCursor::advance()
+{
+    if (position_ + 1 < source_.length()) {
+        return source_.at(position_++);
+    }
+
+    return source_end_mark;
+}
+
+void SourceCursor::skip_while(std::vector<char> const& char_to_skip)
+{
+    for (; position_ < source_.length(); ++position_) {
+        if (std::find(char_to_skip.begin(), char_to_skip.end(), source_[position_]) == char_to_skip.end()) {
+            break;
+        }
+    }
 }
--- src/Support/SourceCursor.hpp
+++ src/Support/SourceCursor.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
 #include <string_view>
+#include <vector>
 
 class SourceCursor {
 public:
     SourceCursor(std::string_view const source);
 
+    char peek(uint8_t const offset);
+    char advance();
+    void skip_while(std::vector<char> const& char_to_skip);
+
 private:
     std::string_view const source_;
+    uint16_t position_;
 };
